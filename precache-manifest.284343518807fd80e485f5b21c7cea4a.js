e4da702953e6ffbece9d416cb78802d50cc61ad1
webDevSaurav <webdevelopersaurav@gmail.com>
2020-06-30 23:32:25 +0530

    Updates

---
+++ precache-manifest.284343518807fd80e485f5b21c7cea4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82206db8c9dfb0170c118cb2b69cfd1f",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "20d69226b039e7488611",
+    "url": "/corona-tracker/static/css/main.55ad37ad.chunk.css"
+  },
+  {
+    "revision": "b0ad297110c3322b9ee9",
+    "url": "/corona-tracker/static/js/2.d9f135c3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.d9f135c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d69226b039e7488611",
+    "url": "/corona-tracker/static/js/main.e3ac96a3.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/corona.d7265326.png"
+  }
+]);
